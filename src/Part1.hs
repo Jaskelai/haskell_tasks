d3a344cf50edfc8e0924c581914192bab7c74f7a
jaskelai <jaskelais@gmail.com>
2020-12-24 20:25:04 +0300

    [Part1]: problem5 fixed

--- src/Part1.hs
+++ src/Part1.hs
@@ -97,6 +97,7 @@ primeFactors :: Integer -> [Integer]
 primeFactors = factorize 2
 
 factorize :: Integer -> Integer -> [Integer]
+factorize _ 1 = [] 
 factorize d n 
    | d * d > n = [n]
    | mod n d == 0 = d : factorize d ( div n d)
